e24ba6b243a9a462b0796f129758238263dacaea
joeseonmi <jemma3136@gmail.com>
2018-01-03 21:31:00 +0900

    [추가] 테마 이미지 추가
    - 아직 적용은 안됨
    - 테마 이미지관련 에셋추가

--- Woosan/Woosan/ThemeViewController.swift
+++ Woosan/Woosan/ThemeViewController.swift
@@ -12,6 +12,16 @@ class ThemeViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    // TODO: - :: 지금은 model과 controller가 다 붙어있는...고쳐야댕
+    
+    let titles:[String] = ["우산 챙기개!(기본)",
+                           "우산 챙겼냥!"]
+    
+    let subscrip:[String] = ["우산챙기개! 기본테마. 강아지가 뛰어댕겨요.",
+                             "얼룩이 고양이가 뛰어댕겨요."]
+    
+    let image:[String] = ["doggythemIcon",
+                          "dungsilcatthemIcon"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,12 +43,14 @@ class ThemeViewController: UIViewController {
 extension ThemeViewController : UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return self.titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ThemeTableViewCell", for: indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ThemeTableViewCell", for: indexPath) as! ThemeTableViewCell
+        cell.themeTitle.text = self.titles[indexPath.row]
+        cell.themeSubsc.text = self.subscrip[indexPath.row]
+        cell.themeImage.image = UIImage(named: self.image[indexPath.row])
         return cell
     }
 }
