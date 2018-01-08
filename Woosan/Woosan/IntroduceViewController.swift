c073f95598e877be58862cf5f390a887429f572f
joeseonmi <jemma3136@gmail.com>
2018-01-08 09:07:02 +0900

    [추가] 개발자에게 메일쓰기
    만든사람 메뉴에서 개발자에게 메일보내기 추가함

--- Woosan/Woosan/IntroduceViewController.swift
+++ Woosan/Woosan/IntroduceViewController.swift
@@ -7,30 +7,50 @@
 //
 
 import UIKit
+import MessageUI
 
-class IntroduceViewController: UIViewController {
+class IntroduceViewController: UIViewController,MFMailComposeViewControllerDelegate {
 
+    @IBAction func sendMail(_ sender: UIButton) {
+        let mailViewcontroller = MFMailComposeViewController()
+        mailViewcontroller.mailComposeDelegate = self
+        mailViewcontroller.setToRecipients(["jemma3136@gmail.com"])
+        mailViewcontroller.setSubject("[우산챙기개]: 제목을 적어주세요")
+        mailViewcontroller.setMessageBody("\n\n\n\n소중한 의견 감사합니다.🤗", isHTML: false)
+        
+        if MFMailComposeViewController.canSendMail(){
+            self.present(mailViewcontroller, animated: true, completion: nil)
+        } else {
+            falseSendMailalert(title: "메일전송 실패", message: "메일 전송에 실패하였습니다.")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "만든 사람"
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        switch result {
+        case .cancelled:
+            print("취소됨")
+        case .failed:
+           print("실패됨")
+        case .saved:
+           print("임시저장됨")
+        case .sent:
+           print("전송됨")
+        }
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    func falseSendMailalert(title text:String, message messageText:String){
+        let alert = UIAlertController.init(title: text, message: messageText, preferredStyle: .alert)
+        let action = UIAlertAction.init(title: "확인", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
+
