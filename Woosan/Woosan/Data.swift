274648ea9323f1ccc422314dc374ecbe023822a2
joeseonmi <jemma3136@gmail.com>
2018-03-10 09:53:28 +0900

    [수정] appkey 수정

--- Woosan/Woosan/Data.swift
+++ Woosan/Woosan/Data.swift
@@ -16,7 +16,7 @@ struct DataShare {
     static let widgetShareDataKey = "group.devjoe.TodayExtensionSharingDefaults"
     static let widgetThemeDataKey = "Theme"
     
-    static let appKey = "dZ3RPoI%2BsacOCxFGAQnh6tn8V3ypiYhPzmRG%2BIY9%2FPq1Xfscm1xJFiC4eimk5GY94zEuMgg8OHJGsusUREKUxg%3D%3D"
+    static let appKey = ""
     static let forecastChoDangi = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastGrib"
     static let forecastSpace = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData"
     static let dustApi = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty"
